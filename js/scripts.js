d3d1faccec106aad0bffc05e5de3422fe4eb50ea
96olbap <alvinwanjala@gmail.com>
2021-12-10 00:47:03 +0300

    This commit will add the multiply function to the calculator

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,8 @@ var subtract = (number1, number2) => number1 - number2;
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Entar another number:"));
 alert(subtract(number1, number2));
+
+var multiply = (number1, number2) => number1 * number2;
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(multiply(number1, number2));
