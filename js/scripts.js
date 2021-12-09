14ab233b072f94268047d187ffc99429bb9113ba
96olbap <alvinwanjala@gmail.com>
2021-12-10 00:46:03 +0300

    This commit will add the subtract function to the calculator

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,8 @@ var add = (number1, number2) => number1 + number2;
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 alert(add(number1, number2));
+
+var subtract = (number1, number2) => number1 - number2;
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Entar another number:"));
+alert(subtract(number1, number2));
